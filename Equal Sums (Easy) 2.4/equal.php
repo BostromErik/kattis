ebed273e39172f87b25211748db3af87af09a7a3
BostromErik <nebostrom@gmail.com>
2022-05-20 17:47:43 +0200

    Started on Equal Sums

--- Equal Sums (Easy) 2.4/equal.php
+++ Equal Sums (Easy) 2.4/equal.php
@@ -1,2 +1,24 @@
 <?php 
-    
+    fscanf(STDIN, "%d", $rows);
+
+    while($i < $rows)
+    {
+        if($i === $rows) break;
+        fscanf(STDIN, "%s", $case);
+        $case = array_shift(explode(' ', $case));
+        $step = 1;
+        $subset = 0;
+        while($j < count($case))
+        {
+            while($k < count($case))
+            {
+                $subset = $subset + ($case[$j] + $case[$j + $step]);
+                if($subset < $case[$j + $step])
+                {
+
+                }
+            }
+            
+        }
+        $i++;
+    }
